3e290337b3d57e748c26c34d24332ace03c9e514
lucyrose93 <lucyrose@protonmail.com>
2017-03-22 11:14:36 +0000

    request module stage one completed

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 "use strict";
 
 const request = require('request');
+const http = require('http');
 
-const myRequest = () => {
-  /*
-  create your own request module here.
-  It should take a url to make a http GET request, and a callback function with three arguments;
-  1. error (String: if an error occurred),
-  2. response(Object; includes the response & statusCode of the request),
-  3. body (String; includes the body of the request)
-  */
+const myRequest = (url, callback) => {
+
+  return http.get(url, function(response) {
+    var body = '';
+    response.on('data', function(chunk){
+      body += chunk;
+    });
+    response.on('end', function(){
+      callback(null, response, body);
+    })
+    }).on('error', function(e){
+      console.log('hey')
+  })
 }
 
-// Helper
+
 const testRequest = (module) => {
   module('http://jsonplaceholder.typicode.com/users/1', function (error, response, body) {
     console.log('error:', error);
@@ -21,8 +27,8 @@ const testRequest = (module) => {
   });
 };
 
-// request module test
-testRequest(request);
-
+// // request module test
+//testRequest(request);
+//
 // // myRequest module test
 testRequest(myRequest);
